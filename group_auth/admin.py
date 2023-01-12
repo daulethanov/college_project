dbc6cff80b289d4212c0431198de7cb54fd5a7b1
alihan <aliz1233773@gmail.com>
2023-01-12 23:44:00 +0600

    12.1.23v4

--- group_auth/admin.py
+++ group_auth/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-from .models import *
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group
+from .models import Users, CustomGroup
+from import_export.admin import ExportActionMixin
 
 
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'group_name')
+class CustomUserAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('username', 'get_group')
+    ordering = ['groups']
 
-    def group_name(self, obj):
-        return obj.groups.name
-    group_name.short_description = 'Group'
+    # def group_name(self, obj):
+    #     return "\n".join([p.users_set for p in obj.groups.filter()])
+    # group_name.short_description = 'Group'
 
 
 admin.site.register(Users, CustomUserAdmin)
+
+
+class CustomGroupAdmin(ExportActionMixin, GroupAdmin):
+    list_display = ('name', 'users_count', )
+    fields = ['name', 'users']
+
+    # def users_count(self, obj):
+    #     pass
+    # users_count.short_description = 'количество юзеров в группе'
+
+
+admin.site.unregister(Group)
+admin.site.register(CustomGroup, CustomGroupAdmin)
