202bce89e4ac45d1fbfeef3a0788691881062d24
alihan <aliz1233773@gmail.com>
2023-01-12 22:04:18 +0600

    12.1.23v2

--- group_auth/admin.py
+++ group_auth/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'group_name')
+
+    def group_name(self, obj):
+        return obj.groups.name
+    group_name.short_description = 'Group'
+
+
+admin.site.register(Users, CustomUserAdmin)
